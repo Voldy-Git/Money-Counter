8ae061e6a7f206189a217a9fb51f364414eae269
Voldy-Git <111450780+Voldy-Git@users.noreply.github.com>
2022-08-28 16:31:42 +0200

    Replacing the program logic and adding user authorization. (There are a couple of bugs to fix before next Sunday)

--- interaction.py
+++ interaction.py
@@ -1,22 +1,19 @@
 import pickle
 
-path_to_dir = r"/home/voldy/amount_money"
-path_to_log = r"/home/voldy/amount_money/log"
-path_to_file = r"/home/voldy/amount_money/log/log.bin"
+support_path = "/log/log.bin"
 
-
-def create_object():
+def create_object(path, sclass):
     money = int(input("Geben Sie den Betrag ihres Geld ein: "))
     minimum = int(input("Geben Sie den Mindestbetrag an, den Sie pro Monat benötigen: "))
-    with open(path_to_file, "wb") as file:
-        pickle.dump(MoneyCounter(money, minimum), file)
+    with open(path + support_path, "wb") as file:
+        pickle.dump(sclass(money, minimum), file)
 
 
-def read_object():
-    with open(path_to_file, "rb") as file:
+def read_object(path):
+    with open(path + support_path, "rb") as file:
         return pickle.load(file)
 
 
-def dump_new_obj():
-    with open(path_to_file, "wb") as file:
-        pickle.dump(create_object(), file)
+def dump_new_obj(path, sclass):
+    with open(path, "wb") as file:
+        pickle.dump(create_object(path, sclass), file)
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import interaction
 import os
 from pathlib import Path
+import pickle
 
 
 class MoneyCounter:
@@ -20,19 +21,66 @@ class MoneyCounter:
         self.money = a_money
 
 
-if not Path(interaction.path_to_dir).is_dir():
-    for dir in (interaction.path_to_dir, interaction.path_to_log):
-        os.mkdir(dir)
+class User:
+    def __init__(self):
+        self.nickname = input("Geben Sie bitte ihre Name: ").rstrip()
+        self.code_name = self.nickname.lower().replace(' ', '-')
+        self.path_to_dir = r"/home/voldy/amount_money/users/"
+        self.path = self.path_to_dir + self.code_name
 
-    interaction.create_object()
-elif not Path(interaction.path_to_log).is_dir():
-    os.mkdir(interaction.path_to_log)
-    interaction.create_object()
-elif not Path(interaction.path_to_file).is_file():
-    interaction.create_object()
+    def authorization(self):
+        if not Path(self.path).is_dir():
+            return self.create_account()
+        else:
+            return self.account()
+
+    def create_account(self):
+        os.mkdir(self.path)
+        password = input("Denken Sie an ihr Passwort: ")
+        with open(self.path + '/password.bin', "wb") as file:
+            pickle.dump(password, file)
+
+        return f"Sie haben ein neues Konto erstellt, herzlichen Glückwunsch. Ihr Passwort lautet {password}.", True
+
+    def account(self):
+        with open(self.path + '/password.bin', "rb") as file:
+            user_password = pickle.load(file)
+
+        max_trying, trying = 5, 1
+        while trying <= max_trying:
+            password = input("Geben Sie ihr Passwort ein: ")
+
+            if user_password == password:
+                return "Passwort ist korrekt anmelden, einloggen...", True
+
+            print(f"Falsches Passwort, versuchen Sie es erneut, Sie haben noch {max_trying - trying}.")
+            trying += 1
+        else:
+            return "Sie haben alle ihre Versuche verbraucht, der Zugang zum System wird verweigert.", False
 
 
-created_object = interaction.read_object()
+def check(path):
+    path_to_log = path + r"/log"
+    if not Path(path).is_dir():
+        for dir in (path, path_to_log):
+            os.mkdir(dir)
 
-created_object.amount(int(input("Geben Sie den Betrag ihres Geld ein: ")))
-print(created_object.comparison())
+        interaction.create_object(path, MoneyCounter)
+    elif not Path(path_to_log).is_dir():
+        os.mkdir(path_to_log)
+        interaction.create_object(path, MoneyCounter)
+    elif not Path(path + path_to_log + "/log.bin").is_file():
+        interaction.create_object(path, MoneyCounter)
+    else:
+        return False
+    return True
+
+
+user = User()
+authorization = user.authorization()
+print(authorization[0])
+
+if authorization[1]:
+    if check(user.path):
+        user_object = interaction.read_object(user.path)
+        print(user_object.comparison())
